a19fa337ab2175119ad41d9886dac3e018874719
lear <1325925707@qq.com>
2019-06-24 10:46:14 +0800

    fix: 使用谷歌作为翻译源

--- core/index.js
+++ core/index.js
@@ -1,26 +1,125 @@
-const axios = require('axios');
+const request = require('request-promise').defaults({
+  simple: false,
+  resolveWithFullResponse: true
+  // proxy:'http://127.0.0.1:8888'
+});
+const urlencode = require('urlencode');
+const iconv = require('iconv-lite');
 
-exports.bdTranslate = async (from = 'zh', to = 'en', words = '测试') => {
-  const payload = {
-    from,
-    to,
-    query: words,
-  }
-  const { data } = await axios({
-    method: 'POST',
-    url: 'https://fanyi.baidu.com/transapi',
+iconv.skipDecodeWarning = true;
+
+let tkk = '429175.1243284773';
+
+async function get(url) {
+  let options = {
+    url: url,
     headers: {
-      'Content-type': 'application/x-www-form-urlencoded'
-    },
-    data: payload,
-    transformRequest: [function (data) {
-      let ret = ''
-      for (let it in data) {
-        ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
-      }
-      return ret
-    }],
-  });
-  
-  return data;
+      'User-Agent':
+        'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Mobile Safari/537.36'
+    }
+  };
+
+  let rsp = await request.get(options);
+
+  if (rsp.statusCode >= 400) {
+    throw 'Translate failed, please check your network.';
+  }
+
+  iconv.decode(rsp.body, 'utf-8');
+
+  return rsp;
 }
+
+// Get Tkk value
+(async () => {
+  let url = 'https://translate.google.cn/';
+  let rsp = await get(url);
+  let tkkMat = rsp.body.match(/tkk:'([\d.]+)'/);
+  tkk = tkkMat ? tkkMat[1] : tkk;
+})();
+
+// translate_m_zh-CN.js:formatted Line 8084
+function Ho(a) {
+  return function() {
+    return a;
+  };
+}
+
+function Io(a, b) {
+  for (var c = 0; c < b.length - 2; c += 3) {
+    var d = b.charAt(c + 2);
+    d = 'a' <= d ? d.charCodeAt(0) - 87 : Number(d);
+    d = '+' == b.charAt(c + 1) ? a >>> d : a << d;
+    a = '+' == b.charAt(c) ? (a + d) & 4294967295 : a ^ d;
+  }
+  return a;
+}
+
+// translate_m_zh-CN.js:formatted Line 8099 fun Ko
+function tk(a, tkk) {
+  var b = tkk || '';
+  var d = Ho(String.fromCharCode(116));
+  var c = Ho(String.fromCharCode(107));
+  d = [d(), d()];
+  d[1] = c();
+  c = '&' + d.join('') + '=';
+  d = b.split('.');
+  b = Number(d[0]) || 0;
+  for (var e = [], f = 0, g = 0; g < a.length; g++) {
+    var k = a.charCodeAt(g);
+    128 > k
+      ? (e[f++] = k)
+      : (2048 > k
+          ? (e[f++] = (k >> 6) | 192)
+          : (55296 == (k & 64512) &&
+            g + 1 < a.length &&
+            56320 == (a.charCodeAt(g + 1) & 64512)
+              ? ((k = 65536 + ((k & 1023) << 10) + (a.charCodeAt(++g) & 1023)),
+                (e[f++] = (k >> 18) | 240),
+                (e[f++] = ((k >> 12) & 63) | 128))
+              : (e[f++] = (k >> 12) | 224),
+            (e[f++] = ((k >> 6) & 63) | 128)),
+        (e[f++] = (k & 63) | 128));
+  }
+  a = b;
+  for (f = 0; f < e.length; f++) (a += e[f]), (a = Io(a, '+-a^+6'));
+  a = Io(a, '+-3^+b+-f');
+  a ^= Number(d[1]) || 0;
+  0 > a && (a = (a & 2147483647) + 2147483648);
+  a %= 1e6;
+  return c + (a.toString() + '.' + (a ^ b));
+}
+
+function getCandidate(tran) {
+  let words = [];
+  if (tran[1]) words = words.concat(tran[1][0][1]);
+  if (tran[5]) words = words.concat(tran[5][0][2].map(t => t[0]));
+  return words;
+}
+exports.googleTranslate = async (from = 'zh-CN', to = 'en', word = '测试') => {
+  let lang = {
+    from,
+    to
+  };
+
+  let url = `https://translate.google.cn/translate_a/single?client=webapp&sl=${
+    lang.from
+  }&tl=${
+    lang.to
+  }&hl=zh-CN&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&pc=1&otf=1&ssel=0&tsel=0&kc=1&tk=${tk(
+    word,
+    tkk
+  )}&q=${urlencode(word, 'utf-8')}`;
+
+  try {
+    let rsp = await get(url);
+    let tranWord = JSON.parse(rsp.body);
+    let candidate = getCandidate(tranWord);
+    return {
+      word: tranWord[0][0][0],
+      candidate
+    };
+  } catch (err) {
+    throw 'Translate failed, please check your network.';
+  }
+};
--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 const minimist = require('minimist');
-const { bdTranslate } = require('./core');
+const { googleTranslate } = require('./core');
 const { isEng } = require('./util');
 const { getHelp } = require('./cmds');
 
 module.exports = async () => {
   const args = minimist(process.argv.slice(2));
   const word = args._[0];
+
   if (args.v || args.version) {
-    console.log('fy-cli version: 0.0.1 - by: flasco');
+    console.log('fy-cli version: 0.0.2 - by: flasco');
   } else if (word == null || args.h || args.help) {
     // 显示帮助
     getHelp();
   } else {
     if (isEng(word)) {
       // 纯字母或者数字, 英文翻译中文
-      const { data } = await bdTranslate('en', 'zh', word);
-      console.log(data[0].dst);
+      const data = await googleTranslate('en', 'zh-CN', word);
+      console.log(data.word);
     } else {
       // 中文翻译英文
-      const { data } = await bdTranslate('zh', 'en', word);
-      console.log(data[0].dst);
+      const data = await googleTranslate('zh-CN', 'en', word);
+      console.log(data.word);
     }
   }
 }
